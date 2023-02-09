1e381055d13f5afa20572d771d6b11b746f1d52d
soyombo <soyombo0@naver.com>
2023-02-09 20:59:07 +0300

    1.000

--- Zailin/ViewController.swift
+++ Zailin/ViewController.swift
@@ -11,40 +11,20 @@ import RxSwift
 
 class ViewController: UIViewController {
     
-    private let tableView: UITableView = {
-        let table = UITableView()
-        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        table.allowsMultipleSelection = true
-        return table
-    }()
-    
-    private var viewModel = MarketViewModel()
-    
-    private var bag = DisposeBag()
+    let tableView = MarketTableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Setting up the View
+        setUpView()
         view.addSubview(tableView)
         tableView.frame = view.bounds
-        bindTableData()
     }
-
-    func bindTableData() {
-        // bind items
-        viewModel.items.bind(to: tableView.rx.items(
-            cellIdentifier: "cell",
-            cellType: UITableViewCell.self))
-        { row, model, cell in
-            cell.textLabel?.text = model.title
-            cell.imageView?.image = UIImage(systemName: model.imageName)
-        }.disposed(by: bag)
-        
-        // model selected handler
-        tableView.rx.modelSelected(Market.self).bind { product in
-            print(product.title)
-        }.disposed(by: bag)
-        // fetch items
-        viewModel.fetchItems()
+    
+    // MARK: - private functions
+    
+    private func setUpView() {
+        view.backgroundColor = .red
     }
 
 }
